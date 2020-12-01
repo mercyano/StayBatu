9964a36295fa5dd0ac25087bfa3aba95cbee1de9
PummelPouch <74892827+PummelPouch@users.noreply.github.com>
2020-12-01 18:23:29 +0700

    Update Booking.php

--- staybatu-site/application/controllers/Booking.php
+++ staybatu-site/application/controllers/Booking.php
@@ -1,22 +1,25 @@
-<?php
+<?php<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 
 class Booking extends CI_Controller
 {
-
-    public function load_wa()
+    public function index()
     {
+        $this->load->view('templates/header');
+        $this->load->view('transaksi/index/');
+        $this->load->view('templates/footer');
     }
 
-    public function addTransaksi1($id = NULL)
+    public function addTransaksi1()
     {
-
         $nama = $this->input->post('nama');
         $email = $this->input->post('email');
         $no_telp = $this->input->post('no_telp');
         $check_in = $this->input->post('check_in');
         $check_out = $this->input->post('check_out');
+        $bukti_transaksi = file_get_contents($_FILES['bukti_transaksi']['tmp_name']);
+        $type = $_FILES['bukti_transaksi']['type'];
 
         $data = array(
             'id_pemesan' => '',
@@ -24,10 +27,13 @@ class Booking extends CI_Controller
             'email' => $email,
             'no_telp' => $no_telp,
             'check_in' => $check_in,
-            'check_out' => $check_out
+            'check_out' => $check_out,
+            'id_transaksi' => '',
+            'bukti_transaksi' => $bukti_transaksi,
+            'mime' => $type
         );
         $this->db->insert('pemesan', $data);
-        redirect('booking/hubung');
+        redirect('host');
     }
     public function addTransaksi2()
     {
