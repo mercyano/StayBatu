cd64c31f21e6ba324dfeab1e2221e4ce6c5c3fd0
Mercyano <60174495+Mercyano@users.noreply.github.com>
2020-12-02 00:27:29 +0700

    Update Home.php
    Update method booking; Add method bayar

--- staybatu-site/application/controllers/Home.php
+++ staybatu-site/application/controllers/Home.php
@@ -39,7 +39,7 @@ class Home extends CI_Controller
 
     public function booking($id = NULL)
     {
-        $data['homestay'] = $this->Home_model->get_homestay($id);
+        $data['homestay'] = $this->Home_model->get_homestay($id); // Mendapatkan tabel homestay dengan 'id' = $id
 
         if (empty($data['homestay'])) {
             show_404();
@@ -50,6 +50,36 @@ class Home extends CI_Controller
 
         $this->load->view('templates/header', $data);
         $this->load->view('booking/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function bayar($id = NULL)
+    {
+        $nama = $this->input->post('nama');
+        $email = $this->input->post('email');
+        $no_telp = $this->input->post('no_telp');
+        $check_in = $this->input->post('check_in');
+        $check_out = $this->input->post('check_out');
+        $bukti_transaksi = file_get_contents($_FILES['bukti_transaksi']['tmp_name']); 
+        $type = $_FILES['bukti_transaksi']['type']; 
+		
+        $data = array(
+            'id_pemesan' => '',
+            'nama' => $nama,
+            'email' => $email,
+            'no_telp' => $no_telp,
+            'check_in' => $check_in,
+            'check_out' => $check_out,
+            'bukti_transaksi' => $bukti_transaksi,
+            'mime' => $type
+        );        
+        $this->db->insert('pemesan', $data);
+
+        $data['homestay'] = $this->Home_model->get_homestay($id);
+        $data['harga'] = $data['homestay']['harga'];
+
+        $this->load->view('templates/header');
+        $this->load->view('booking/bayar', $data);
         $this->load->view('templates/footer');
     }
 
