70ff28437c1a65b524453002638f5b449f12759e
Mercyano <60174495+Mercyano@users.noreply.github.com>
2020-12-01 13:03:55 +0700

    Update Home_model.php

--- staybatu-site/application/models/Home_model.php
+++ staybatu-site/application/models/Home_model.php
@@ -11,11 +11,22 @@ class Home_model extends CI_Model
 
     public function get_homestay($id = FALSE)
     {
-        if ($id == FALSE) {
-            $query = $this->db->get('homestay');
-            return $query->result_array();
-        }
-        $query = $this->db->get_where('homestay', array('id' => $id));
-        return $query->row_array();
+        if($id == FALSE){
+                $query = $this->db->get('homestay');
+                return $query->result_array();
+            }
+            $query = $this->db->get_where('homestay', array('id'=>$id));
+            return $query->row_array();
     }
+	
+	public function get_recommend_homestay($id = FALSE){
+            if($id == FALSE){
+                $this->db->order_by('homestay.harga', 'INCREMENT');
+                $query = $this->db->get('homestay', 11);
+                
+                return $query->result_array();
+            }
+            $query = $this->db->get_where('homestay', array('id'=>$id));
+            return $query->row_array();
+        }
 }
