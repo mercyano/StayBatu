5ec7d4dce524ce03b944fca123407dd506e36d45
PummelPouch <74892827+PummelPouch@users.noreply.github.com>
2020-12-02 10:33:22 +0700

    Update index.php

--- staybatu-site/application/views/booking/index.php
+++ staybatu-site/application/views/booking/index.php
@@ -3,30 +3,23 @@
     <center>
         <h3>Booking <?php echo $homestay['judul'] ?></h3>
         <hr>
-        <?= form_open_multipart('home/bayar/'.$homestay['id'])  ?>
+        <?= form_open_multipart('home/bayar/' . $homestay['id'])  ?>
 
-            <div class="form-group">
-                <label>Nama : <input type="text" class="form-control" name="nama" id="nama" placeholder="Tulis nama" required> </label>
-            </div>
-            <div class="form-group">
-                <label>Email : <input type="text" class="form-control" name="email" id="email" placeholder="Tulis Email" required></label>
-            </div>
-            <div class="form-group">
-                <label>No Telepon : <input type="text" class="form-control" name="no_telp" id="no_telp" placeholder="No telepon" required></label>
-            </div>
-            <div class="form-group">
-                <label>Check in : <input type="date" class="form-control" name="check_in" id="check_out" required></label>
-            </div>
-            <div class="form-group">
-                <label>Check out : <input type="date" class="form-control" name="check_out" id="check_out" required></label>
-            </div>
-            <div class="custom-file">
-                <input type="file" accept=".jpg, .jpeg, .png" class="custom-file-input" name="bukti_transaksi" id="bukti_transaksi" required>
-                <label class="custom-file-label" for="bukti_transaksi">Screenshoot Bukti Pembayaran <span style="color: red;">*</span></label></label>
-                <small id="bukti_transaksi" class="text-muted">
-                    Pembayaran dapat dilakukan dengan menghubungi nomor whatsapp berikut : <strong><?php echo $homestay['no_wa'] ?></strong>
-                </small>
-            </div>
+        <div class="form-group">
+            <label>Nama : <input type="text" class="form-control" name="nama" id="nama" placeholder="Tulis nama" required> </label>
+        </div>
+        <div class="form-group">
+            <label>Email : <input type="text" class="form-control" name="email" id="email" placeholder="Tulis Email" required></label>
+        </div>
+        <div class="form-group">
+            <label>No Telepon : <input type="text" class="form-control" name="no_telp" id="no_telp" placeholder="No telepon" required></label>
+        </div>
+        <div class="form-group">
+            <label>Check in : <input type="date" class="form-control" name="check_in" id="check_out" required></label>
+        </div>
+        <div class="form-group">
+            <label>Check out : <input type="date" class="form-control" name="check_out" id="check_out" required></label>
+        </div>
 
 </div>
 <br>
