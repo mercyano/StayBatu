863f546ecaecd3e9a1348a388e96ec5fa2b8b6b9
tunggul30 <75293067+tunggul30@users.noreply.github.com>
2020-12-02 19:14:30 +0700

    Add files via upload

--- staybatu-site/application/views/templates_login/sidebar.php
+++ staybatu-site/application/views/templates_login/sidebar.php
@@ -36,18 +36,18 @@
 
             <li class="nav-item dropdown">
                 <a class="nav-link" href="<?= base_url('admin/homestay') ?>">
-                    <i class="fas fa-fw fa-boxes"></i>
+                    <i class="fas fa-fw fa-hotel"></i>
                     <span>Homestay</span>
                 </a>
             </li>
             <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Reservasi</span>
+                    <i class="fas fa- fw fa-money-bill-wave"></i>
+                    <span>Pembayaran</span>
                   </a>
                   <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-                    <a class="dropdown-item" href="<?= base_url('admin/reservasi'); ?>">Reservasi</a>
-                    <a class="dropdown-item" href="<?= base_url('admin/reservasi_pembayaran'); ?>">Reservasi Pembayaran</a>
+                    <a class="dropdown-item" href="<?= base_url('admin/reservasi'); ?>">Booking</a>
+                    <a class="dropdown-item" href="<?= base_url('admin/reservasi_pembayaran'); ?>">Transaksi</a>
                   </div>
             </li>
             <li class="nav-item dropdown">
