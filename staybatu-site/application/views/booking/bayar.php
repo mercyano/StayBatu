f1cce3c2d67b6ce45e789034a252a5451de8e475
PummelPouch <74892827+PummelPouch@users.noreply.github.com>
2020-12-02 13:58:17 +0700

    Update bayar.php

--- staybatu-site/application/views/booking/bayar.php
+++ staybatu-site/application/views/booking/bayar.php
@@ -1,31 +1,39 @@
-<?php 
-    function dateDiffInDays($date1, $date2)  
-    { 
-        // Calculating the difference in timestamps 
-        $diff = strtotime($date2) - strtotime($date1); 
-          
-        // 1 day = 24 hours 
-        // 24 * 60 * 60 = 86400 seconds 
-        return abs(round($diff / 86400)); 
-    }
+<?php
+function dateDiffInDays($date1, $date2)
+{
+    // Calculating the difference in timestamps 
+    $diff = strtotime($date2) - strtotime($date1);
+
+    // 1 day = 24 hours 
+    // 24 * 60 * 60 = 86400 seconds 
+    return abs(round($diff / 86400));
+}
 ?>
 
 <div class="container">
     <center>
-        <p>Check In pada : <?= $check_in;?></p><br>
-        <p>Check Out pada : <?= $check_out;?></p>
-        <p>Biaya per Malam : Rp. <?= $harga?></p>
-        <p>Total yang harus  dibayar : Rp. <?= $harga * (dateDiffInDays($check_in, $check_out))?></p>
-        <!-- <?php form_open_multipart('home/uploadpembayaran'); ?> -->
+
+        <h3>Verifikasi Bukti Pembayaran</h3>
+        <hr>
+
+        <p>Check In pada : <?= $check_in; ?></p>
+        <p>Check Out pada : <?= $check_out; ?></p>
+        <p>Biaya per Malam : Rp. <?= $harga ?></p>
+        <p>Total yang harus dibayar : Rp. <?= $harga * (dateDiffInDays($check_in, $check_out)) ?></p>
+
+        <?= form_open_multipart('home/transaksi/' . $homestay['id'])  ?>
         <div class="custom-file">
-                <input type="file" accept=".jpg, .jpeg, .png" class="custom-file-input" name="bukti_transaksi" id="bukti_transaksi" required>
-                <label class="custom-file-label" for="bukti_transaksi">Screenshoot Bukti Pembayaran <span style="color: red;">*</span></label></label>
-                <small id="bukti_transaksi" class="text-muted">
-                    Pembayaran dapat dilakukan dengan menghubungi nomor whatsapp berikut : <strong><?php echo $homestay['no_wa'] ?></strong>
-                </small>
+            <input type="file" accept=".jpg, .jpeg, .png" class="custom-file-input" name="bukti_transaksi" id="bukti_transaksi" required>
+            <label class="custom-file-label" for="bukti_transaksi">Screenshoot Bukti Pembayaran <span style="color: red;">*</span></label></label>
+            <small id="bukti_transaksi" class="text-muted">
+                Pembayaran dapat dilakukan dengan menghubungi nomor whatsapp berikut : <strong><?php echo $homestay['no_wa'] ?></strong>
+            </small>
         </div>
-            <button type=" submit" class="btn btn-success">Submit</button>
-        </form>
+        <br>
+        <br>
+        <button type="submit" class="btn btn-success">Submit</button>
+        <br>
+        <br>
+        <?= form_close() ?>
     </center>
 </div>
-
