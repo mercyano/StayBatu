9a5003213d92a06881b875e41a5c079801f4559a
tunggul30 <75293067+tunggul30@users.noreply.github.com>
2020-12-03 14:18:27 +0700

    Add files via upload

--- staybatu-site/application/models/Kamar_model.php
+++ staybatu-site/application/models/Kamar_model.php
@@ -38,4 +38,17 @@ class Kamar_model extends CI_Model
          
          return $query;
 	}
+
+   public function get_pemesan($data){ // Menangkap id_pemilik USER
+        // $array = array('pemesan.id_pemesan' => 'upload_transaksi.id_transaksi', 'pemesan.id_pemilik' => $data);
+        $query = $this->db->select('pemesan.id_pemesan, pemesan.id_pemilik, homestay.judul, pemesan.id_homestay, pemesan.nama, pemesan.check_in, pemesan.check_out, pemesan.no_telp, upload_transaksi.bukti_transaksi')
+         ->from('pemesan')
+         ->join('homestay', 'homestay.id_homestay = pemesan.id_homestay')
+         ->join('user', 'pemesan.id_pemilik = user.id_pemilik')
+         ->join('upload_transaksi', 'pemesan.id_pemesan = upload_transaksi.id_transaksi')
+         ->where('pemesan.id_pemilik', $data)
+        //  ->where('pemesan.id_pemesan', 'upload_transaksi.id_transaksi')
+         ->get()->result_array();
+         return $query;
+    }
 }
