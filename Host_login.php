3e21f6f8a0420afdef0ea2c74aaddfee0a18d039
tunggul30 <75293067+tunggul30@users.noreply.github.com>
2020-12-05 03:04:39 +0700

    Add files via upload

---
+++ Host_login.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Host_login extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+		$this->load->model('Home_model');
+    }
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('host_login/index', $data);
+    }
+
+    public function sewakan()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('host_login/host_sewa',$data);
+    }
+	
+	public function edit($id = NULL){
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['homestay'] = $this->Home_model->get_homestay($id);
+
+        if(empty($data['homestay'])){
+            show_404();
+        }
+
+        $data['title'] = 'Edit Homestay';
+        $this->load->view('host_login/edit', $data);
+
+   }
+
+   public function deleteHomestay($id) {
+        $this->db->delete('homestay', array('id' => $id));
+        redirect('user/kamar_users');
+    }
+
+}
