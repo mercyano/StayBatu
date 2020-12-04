7c5071fb35501052695d132b0751d5c88b33392b
Mercyano <mercyanodandi@gmail.com>
2020-12-05 00:56:04 +0700

    Update homestay.php
    Update tabel homestay

--- staybatu-site/application/views/admin_login/homestay.php
+++ staybatu-site/application/views/admin_login/homestay.php
@@ -12,7 +12,6 @@
                   <thead>
                     <tr>
                       <th>No</th>
-                      <th>Id</th>
                       <th>Judul</th>
                       <th>Harga</th>
                       <th>Kapasitas</th>
@@ -23,7 +22,6 @@
                       <th>AC</th>
                       <th>Dapur</th>
                       <th>Parkir</th>
-                      <th>Host Id</th>
                       <th>Status</th>
                       <th>Aksi</th>
                     </tr>
@@ -33,19 +31,17 @@
                     <?php foreach( $tabel as $homestay ): ?>
                     <tr>
                       <td><?= $i; ?></td>
-                      <td><?= $homestay['id_homestay']; ?></td>
                       <td><?= $homestay['judul']; ?></td>
                       <td><?= $homestay['harga']; ?></td>
                       <td><?= $homestay['kapasitas']; ?></td>
                       <td><?= $homestay['kamar_tidur']; ?></td>
                       <td><?= $homestay['kamar_mandi']; ?></td>
-                      <td><?= $homestay['wifi']; ?></td>
-                      <td><?= $homestay['tv']; ?></td>
-                      <td><?= $homestay['ac']; ?></td>
-                      <td><?= $homestay['dapur']; ?></td>
-                      <td><?= $homestay['parkir']; ?></td>
-                      <td><?= $homestay['id_pemilik']; ?></td>
-                      <td><?= $homestay['statushs']; ?></td>
+                      <td><?php echo ($homestay['wifi'] == 1) ? ('✔') : ('✘') ?></td>
+                      <td><?php echo ($homestay['tv'] == 1) ? ('✔') : ('✘') ?></td>
+                      <td><?php echo ($homestay['ac'] == 1) ? ('✔') : ('✘') ?></td>
+                      <td><?php echo ($homestay['dapur'] == 1) ? ('✔') : ('✘') ?></td>
+                      <td><?php echo ($homestay['parkir'] == 1) ? ('✔') : ('✘') ?></td>
+                      <td><?php echo ($homestay['statushs'] == 0) ? ('Available') : ('Not Available') ?></td>
                       <td>
                         <center>
                           <a href="<?= base_url('admin/deleteHomestay/'). $homestay['id_homestay']; ?>">
