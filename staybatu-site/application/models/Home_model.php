225806ae469554081590ffc445a48a6af822bf2c
tunggul30 <75293067+tunggul30@users.noreply.github.com>
2020-12-04 22:13:32 +0700

    Add files via upload

--- staybatu-site/application/models/Home_model.php
+++ staybatu-site/application/models/Home_model.php
@@ -11,7 +11,7 @@ class Home_model extends CI_Model
     public function get_homestay($id = FALSE)
     {
         if($id == FALSE){
-                $query = $this->db->get('homestay');
+                $query = $this->db->get_where('homestay',array('statushs'=>0));
                 return $query->result_array();
             }
             $query = $this->db->get_where('homestay', array('id_homestay'=>$id));
@@ -21,7 +21,7 @@ class Home_model extends CI_Model
 	public function get_recommend_homestay($id = FALSE){
             if($id == FALSE){
                 $this->db->order_by('homestay.harga', 'INCREMENT');
-                $query = $this->db->get('homestay', 11);
+                $query = $this->db->get_where('homestay', array('statushs'=>'0'),12);
                 
                 return $query->result_array();
             }
