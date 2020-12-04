25e56458f73f4b82a63e201edaf199d0368ba62e
tunggul30 <75293067+tunggul30@users.noreply.github.com>
2020-12-05 04:56:35 +0700

    Add files via upload

--- staybatu-site/application/views/about/index.php
+++ staybatu-site/application/views/about/index.php
@@ -1,6 +1,48 @@
-<h2>About</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida ex eu imperdiet eleifend. Suspendisse
-	finibus nibh nec augue lacinia, a elementum ipsum elementum. Maecenas nec aliquam orci, quis sodales nunc. Duis
-	neque eros, dignissim nec odio vitae, mollis consectetur orci. Proin mattis sit amet turpis eget commodo. Maecenas
-	lobortis congue massa, eget tempor nunc pharetra ac. Praesent quis tempor quam. Quisque maximus, purus quis
-	fermentum consectetur, magna metus semper felis, vel sollicitudin ligula ligula non libero.</p>
+
+<div class="container-fluid">
+  <h2>Layanan</h2>
+  <h4>What we offer</h4>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+    <i class="fas fa-bed" style="font-size: 100px;"></i>		
+      <h4>Menyewa Homestay</h4>
+      <p>Kami menawarkan informasi tentang homestay dan jika anda berminat, anda dapat menyewa homestay yang kami tawarkan</p>
+    </div>
+    <div class="col-sm-4">
+    <i class="fas fa-home" style="font-size: 100px;"></i>
+      <h4>Sewakan Homestaymu</h4>
+      <p>Kalian dapat meyewakan homestay yang kalian miliki di website kami</p>
+    </div>
+    <div class="col-sm-4">
+	<i class="fas fa-cogs" style="font-size: 100px;"></i>
+      <h4>Operasikan Homestaymu</h4>
+      <p>Kalian dapat mengoperasikan homestay yang telah kalian daftarkan kepada website kami</p>
+    </div>
+  </div>
+</div>
+<div class="container-fluid">
+
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>About</h2><br>
+      <h4>Tentang Website Ini.</h4><br>
+      <p>Website ini dibuat untuk menyediakan informasi kepada masyarakat tentang homestay yang ada di Batu. Homestay adalah salah satu bentuk penginapan yang populer. Para pengunjung atau tamu menginap di kediaman penduduk setempat di kota tempat mereka bepergian. Lama tinggal dapat bervariasi dari satu malam hingga lebih dari setahun. Penginapan dapat diberikan secara gratis, dan sebagai ganti untuk kompensasi uangnya dapat saling menginap (bertukar) pada rumah asal si penginap, atau sebagai gantinya dapat pula berupa membantu membersihkan rumah atau bekerja di properti tuan rumah. Inap keluarga adalah contoh dari konsumsi dan pembagian kolaboratif. </p>
+
+    </div>
+    <div class="col-sm-4">
+      <i class="fas fa-chart-bar" style="font-size: 250px; "></i>
+    </div>
+  </div>
+  <br/><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <i class="fas fa-globe-asia" style="font-size: 250px; "></i>
+    </div>
+    <div class="col-sm-8">
+      <h2>Visi Misi</h2><br>
+      <h4><strong>VISI:</strong> Visi kami adalah untuk menyediakan informasi homestay kepada masyarakat yang ingin menyewa homestay di Batu.</h4><br>
+      <p><strong>MISI:</strong> a. Misi kami adalah menyediakan tempat untuk menaruh informasi itu kedalam sebuah media elektronik yang dapat diakses oleh siapa saja b. Mewujudkan Service Quality dengan integritas yang tinggi c. Menjadikan diferensiasi sebagai kekuatan untuk berkembang d. Berkolaborasi dengan para pelaku bisnis demi memperkuat pondasi bisnis yang telah dibangun.</p>
+    </div>
+  </div>
+</div>
