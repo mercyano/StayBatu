5d13742bc23ddf11c6aa5ffcd402808bf4662f6b
Mercyano <mercyanodandi@gmail.com>
2020-12-05 01:09:25 +0700

    Update sidebar.php

--- staybatu-site/application/views/templates_login/sidebar.php
+++ staybatu-site/application/views/templates_login/sidebar.php
@@ -38,7 +38,7 @@
             <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa- fw fa-money-bill-wave"></i>
-                    <span>Pembayaran</span>
+                    <span>Pemesanan</span>
                   </a>
                   <div class="dropdown-menu" aria-labelledby="pagesDropdown">
                     <a class="dropdown-item" href="<?= base_url('admin/reservasi'); ?>">Booking</a>
