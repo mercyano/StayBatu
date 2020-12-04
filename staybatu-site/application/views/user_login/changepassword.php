fba4a6d3b04c7f1f0bf1d249cd75ccfdec51b1b2
tunggul30 <75293067+tunggul30@users.noreply.github.com>
2020-12-04 17:10:39 +0700

    Add files via upload

--- staybatu-site/application/views/user_login/changepassword.php
+++ staybatu-site/application/views/user_login/changepassword.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-lg-6">
             <?= $this->session->flashdata('message'); ?>
-            <form action="<?= base_url('user/changepassword'); ?>" method="post">
+            <form action="<?= base_url('user/user_changepassword'); ?>" method="post">
                 <div class="form-group">
                     <label for="current_password">Current Password</label>
                     <input type="password" class="form-control" id="current_password" name="current_password">
